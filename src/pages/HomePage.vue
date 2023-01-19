3f69e687cad0c3d73b12e36d97b5ba37b89540a8
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-01-20 01:34:12 +0300

    base-section add draggable component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,21 +6,31 @@
       </a-button>
       <a-row :gutter="16">
         <base-section title="Yapılacaklar">
-          <base-section-item v-for="item in todo" :key="item.id" :item="item" />
+          <draggable :list="todos" group="todosapp">
+            <base-section-item
+              v-for="item in todos"
+              :key="item.id"
+              :item="item"
+            />
+          </draggable>
         </base-section>
         <base-section title="Yapılıyor">
-          <base-section-item
-            v-for="item in inProgress"
-            :key="item.id"
-            :item="item"
-          />
+          <draggable :list="inProgress" group="todosapp">
+            <base-section-item
+              v-for="item in inProgress"
+              :key="item.id"
+              :item="item"
+            />
+          </draggable>
         </base-section>
         <base-section title="Yapıldı">
-          <base-section-item
-            v-for="item in completed"
-            :key="item.id"
-            :item="item"
-          />
+          <draggable :list="completed" group="todosapp">
+            <base-section-item
+              v-for="item in completed"
+              :key="item.id"
+              :item="item"
+            />
+          </draggable>
         </base-section>
       </a-row>
     </div>
@@ -30,63 +40,66 @@
 <script>
 import BaseSection from "../components/BaseSection.vue";
 import BaseSectionItem from "../components/BaseSectionItem.vue";
+import draggable from "vuedraggable";
+
 export default {
   name: "HomePage",
   components: {
     BaseSection,
     BaseSectionItem,
+    draggable,
   },
   data() {
     return {
-      todo: [
+      todos: [
         {
           id: 1,
-          title: "Title 1",
-          content: "Content 1",
+          title: "todo 1",
+          content: "todo 1",
         },
         {
           id: 2,
-          title: "Title 2",
-          content: "Content 2",
+          title: "todo 2",
+          content: "todo 2",
         },
         {
           id: 3,
-          title: "Title 3",
-          content: "Content 3",
+          title: "todo 3",
+          content: "todo 3",
         },
       ],
       inProgress: [
         {
           id: 1,
-          title: "Title 1",
-          content: "Content 1",
+          title: "progress 1",
+          content: "progress 1",
         },
         {
           id: 2,
-          title: "Title 2",
-          content: "Content 2",
+          title: "progress 2",
+          content: "progress 2",
         },
         {
           id: 3,
-          title: "Title 3",
-          content: "Content 3",
+          title: "progress 3",
+          content: "progress 3",
         },
       ],
       completed: [
         {
           id: 1,
-          title: "Title 1",
-          content: "Content 1",
+          title: "completed 1",
+          content: "completed 1",
         },
         {
           id: 2,
-          title: "Title 2",
-          content: "Content 2",
+          title: "completed 2",
+          content: "completed 2",
         },
         {
           id: 3,
-          title: "Title 3",
-          content: "Content 3",
+          title: "completed 3",
+          content: "completed 3",
         },
       ],
     };
