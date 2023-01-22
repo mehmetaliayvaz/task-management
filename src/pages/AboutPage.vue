5e2f5a643ea7a47ca1fad98df17198b0fc425bb1
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-01-22 23:01:19 +0300

    mobile style fixed

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="wrapper">
-    <div class="container">AboutPage</div>
+    <div class="container">
+      <div class="about-section">
+        <p class="about-section-text">
+          With Task-Management, you can plan your tasks and take notes. Your
+          tasks are saved in localstorage. With the Task-Management application
+          you can:
+        </p>
+        <a-list item-layout="horizontal" bordered style="display: inline-block">
+          <a-list-item>Creating a section</a-list-item>
+          <a-list-item>Creating a task</a-list-item>
+          <a-list-item>Editing a section</a-list-item>
+          <a-list-item>Deleting a section</a-list-item>
+          <a-list-item>Relocating tasks</a-list-item>
+          <a-list-item>Deleting a task</a-list-item>
+        </a-list>
+      </div>
+      <div class="about-section">
+        <p class="about-section-text">
+          The following technologies are used in the Task-Management
+          application:
+        </p>
+        <a-list item-layout="horizontal" bordered style="display: inline-block">
+          <a-list-item>Vue2</a-list-item>
+          <a-list-item>Vue Router</a-list-item>
+          <a-list-item>Vuex</a-list-item>
+          <a-list-item>Vue Hero Icons</a-list-item>
+          <a-list-item>Ant Design Vue</a-list-item>
+          <a-list-item>Vue Draggable</a-list-item>
+        </a-list>
+      </div>
+    </div>
   </div>
 </template>
 
